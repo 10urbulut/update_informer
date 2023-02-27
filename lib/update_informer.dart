471c82b2557a43c3ae4115368cefab81a5836ed9
Onur Bulut <77685360+10urbulut@users.noreply.github.com>
2023-02-27 17:52:52 +0300

    Update update_informer.dart

--- lib/update_informer.dart
+++ lib/update_informer.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
+
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:update_informer/src/app_store_model/app_store_model.dart';
 import 'package:flutter/material.dart';
@@ -12,7 +12,7 @@ import 'package:provider/provider.dart';
 import 'package:store_redirect/store_redirect.dart';
 
 part 'src/api_service.dart';
-part 'src/update_informer.dart';
+part 'src/update_informer_widget.dart';
 part 'src/update_informer_manager.dart';
 
 typedef JsonType = Map<String, dynamic>;
