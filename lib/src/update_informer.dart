8c4890534c210912697eb439d7670d31ccc297ef
Onur Bulut <77685360+10urbulut@users.noreply.github.com>
2023-02-27 15:07:44 +0300

    Update update_informer.dart

--- lib/src/update_informer.dart
+++ lib/src/update_informer.dart
@@ -1,4 +1,6 @@
-part of update_informer;
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:update_informer/src/update_informer_manager.dart';
 
 typedef JsonType = Map<String, dynamic>;
 
