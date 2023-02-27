9f376fa950eecbb2355fe8e7652641f4d0cfadd5
Onur Bulut <77685360+10urbulut@users.noreply.github.com>
2023-02-27 17:35:32 +0300

    Update update_informer_test.dart

--- test/update_informer_test.dart
+++ test/update_informer_test.dart
@@ -1,5 +1,16 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:update_informer/src/update_informer_manager.dart';
+import 'package:update_informer/update_informer.dart';
 
 void main() {
-  test('adds one to input values', () {});
+  const UpdateInformerWidget(
+    androidAppBundle: "",
+    iosAppBundle: "",
+    iosAppId: "",
+  );
+  var provider = UpdateInformerManager("");
+
+  test('Check version', () {
+    expect(false, provider.versionIsUpToDate);
+  });
 }
