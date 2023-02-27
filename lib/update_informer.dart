9d04b3ebf6edab8bda9336ba7f38d16252171e8c
Onur Bulut <77685360+10urbulut@users.noreply.github.com>
2023-02-27 15:07:49 +0300

    Update update_informer.dart

--- lib/update_informer.dart
+++ lib/update_informer.dart
@@ -1,7 +1,3 @@
 library update_informer;
 
-import 'package:flutter/material.dart';
-import 'package:update_informer/src/update_informer_manager.dart';
-import 'package:provider/provider.dart';
 export 'update_informer.dart';
-part 'src/update_informer.dart';
