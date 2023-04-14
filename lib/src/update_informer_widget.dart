531bed36a3784662b5ac268736526385771d250c
Onur Bulut <77685360+10urbulut@users.noreply.github.com>
2023-04-14 12:05:00 +0300

    Add set state

--- lib/src/update_informer_widget.dart
+++ lib/src/update_informer_widget.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:store_redirect/store_redirect.dart';
 
 import '../update_informer.dart';
 
-class UpdateInformerWidget extends StatelessWidget {
+class UpdateInformerWidget extends StatefulWidget {
   const UpdateInformerWidget({
     Key? key,
     required this.androidAppBundle,
@@ -22,47 +23,86 @@ class UpdateInformerWidget extends StatelessWidget {
   final double? radius;
 
   @override
+  State<UpdateInformerWidget> createState() => _UpdateInformerWidgetState();
+}
+
+class _UpdateInformerWidgetState extends State<UpdateInformerWidget> {
+  @override
+  void initState() {
+    initialize(widget.iosAppBundle);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => UpdateInformerManager(iosAppBundle),
-      builder: (context, _) => Visibility(
-        visible: !context.watch<UpdateInformerManager>().versionIsUpToDate,
-        child: InkWell(
-          onTap: () => context.read<UpdateInformerManager>().upToDateOnTap(
-              iosAppBundle: iosAppBundle,
-              androidAppBundle: androidAppBundle,
-              iosAppId: iosAppId),
-          child: Container(
-            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-            decoration: BoxDecoration(
-              color: backgroundColor ?? Colors.amber.shade700,
-              borderRadius: BorderRadius.circular(radius ?? 15),
-            ),
-            padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  informationText ??
-                      "Yeni versiyon mevcut. Yüklemek için dokunun...",
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodySmall
-                      ?.copyWith(color: Colors.white),
-                ),
-                Icon(
-                  Icons.touch_app_rounded,
-                  color: Theme.of(context)
-                      .iconTheme
-                      .copyWith(color: Colors.white)
-                      .color,
-                  size: 20,
-                )
-              ],
-            ),
+    return Visibility(
+      visible: !versionIsUpToDate,
+      child: InkWell(
+        onTap: () => upToDateOnTap(
+            iosAppBundle: widget.iosAppBundle,
+            androidAppBundle: widget.androidAppBundle,
+            iosAppId: widget.iosAppId),
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+          decoration: BoxDecoration(
+            color: widget.backgroundColor ?? Colors.amber.shade700,
+            borderRadius: BorderRadius.circular(widget.radius ?? 15),
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                widget.informationText ??
+                    "Yeni versiyon mevcut. Yüklemek için dokunun...",
+                style: Theme.of(context)
+                    .textTheme
+                    .bodySmall
+                    ?.copyWith(color: Colors.white),
+              ),
+              Icon(
+                Icons.touch_app_rounded,
+                color: Theme.of(context)
+                    .iconTheme
+                    .copyWith(color: Colors.white)
+                    .color,
+                size: 20,
+              )
+            ],
           ),
         ),
       ),
     );
   }
+
+  void initialize(iosBundleId) async {
+    await checkAppversion(iosBundleId);
+    debugPrint("Init worked");
+  }
+
+  bool _versionIsUpToDate = true;
+  final ApiService _apiService = ApiService();
+
+  Future<void> upToDateOnTap({
+    required String iosAppBundle,
+    required String androidAppBundle,
+    required String iosAppId,
+  }) async {
+    await StoreRedirect.redirect(
+      androidAppId: androidAppBundle,
+      iOSAppId: iosAppId,
+    );
+
+    await checkAppversion(iosAppBundle);
+  }
+
+  Future<void> checkAppversion(String iosBundleId) async {
+    _versionIsUpToDate = await _apiService.checkAppstoreVersion(iosBundleId);
+
+    if (!_versionIsUpToDate) {
+      setState(() {});
+    }
+  }
+
+  bool get versionIsUpToDate => _versionIsUpToDate;
 }
