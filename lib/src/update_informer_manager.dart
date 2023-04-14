dda508ebb0ee226b0374a746dc4392d6d8352740
Onur Bulut <77685360+10urbulut@users.noreply.github.com>
2023-04-14 12:34:24 +0300

    Add set state

--- lib/src/update_informer_manager.dart
+++ lib/src/update_informer_manager.dart
@@ -3,6 +3,38 @@ import 'package:store_redirect/store_redirect.dart';
 
 import '../update_informer.dart';
 
-class UpdateInformerManager {
-  UpdateInformerManager(String iosBundleId) {}
+class UpdateInformerManager extends ChangeNotifier {
+  UpdateInformerManager(String iosBundleId) {
+    initialize(iosBundleId);
+  }
+  void initialize(iosBundleId) async {
+    await checkAppversion(iosBundleId);
+    debugPrint("Init worked");
+  }
+
+  bool _versionIsUpToDate = true;
+  final ApiService _apiService = ApiService();
+
+  Future<void> upToDateOnTap({
+    required String iosAppBundle,
+    required String androidAppBundle,
+    required String iosAppId,
+  }) async {
+    await StoreRedirect.redirect(
+      androidAppId: androidAppBundle,
+      iOSAppId: iosAppId,
+    );
+
+    await checkAppversion(iosAppBundle);
+  }
+
+  Future<void> checkAppversion(String iosBundleId) async {
+    _versionIsUpToDate = await _apiService.checkAppstoreVersion(iosBundleId);
+
+    if (!_versionIsUpToDate) {
+      notifyListeners();
+    }
+  }
+
+  bool get versionIsUpToDate => _versionIsUpToDate;
 }
