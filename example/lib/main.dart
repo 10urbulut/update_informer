df21c15d48d6641768b47d41539c04983c79f74e
Onur Bulut <10urbulut@gmail.com>
2023-08-14 10:23:30 +0300

    Add example

---
+++ example/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import 'package:update_informer/update_informer.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Stack(
+          children: [
+            UpdateInformerWidget(
+                androidAppBundle: "your android app bundle here",
+                iosAppBundle: "your ios app bundle here",
+                iosAppId: "your app id here"),
+            Center(
+              child: Text('Hello World. Is my app up to date?'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
